3aaf62011e3be931cafc2ee621e1882eb2829575
Atique Morshed Sami <iamsamix17@gmail.com>
2021-07-17 11:59:27 +0600

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors);
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Server is running on port ${PORT}`))
+  .catch((error) => console.log(error.message));
